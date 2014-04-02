16a7392b3b8d669d2dc744aa1739ae4a7be85c5f
Brian Taylor <btaylor@umbc.edu>
2014-04-02 16:19:15 -0400

    mostly empty circular.c

---
+++ project6/circular.c
@@ -0,0 +1,26 @@
+#include "circular.h"
+
+/*
+ * This is where you should write all your code (except any additional
+ * tests you choose to include).
+ */
+
+
+/*
+ * First define what the real _cbuf struct should look like. Remember,
+ * this is opaque to the users of your library so you can do whatever
+ * you like in here. The _cbuf struct is typedefd in the header to the
+ * cbuf type so you're free to use either type mostly
+ * interchangably. The one exception is that sizeof calls must use the
+ * _cbuf type.
+ */
+
+struct _cbuf {
+  // whatever you want can go in here
+};
+
+/*
+ * Now define your implementations of all of the functions given in
+ * the header. The test programs will not link successfully until each
+ * function they use is defined here.
+ */
